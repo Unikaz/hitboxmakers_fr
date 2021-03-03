ddc1d1c0f1699f1a207bb5e271f4143d59273ec0
Unikaz <unikaz@free.fr>
2021-03-03 19:55:16 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://hitboxmakers.fr/">
+  <meta property="og:title" content="Hitbox Makers">
+  <meta property="og:description" content="48h pour faire un jeu">
+  <meta property="og:image" content="http://hitboxmakers.fr/img/animationHitBox.gif">
+
+  <link rel="stylesheet" href="style.css">
+  <link rel="icon" type="image/png" href="favicon.png">
+  <title>Hitbox Makers</title>
+  <meta name="description" content="48h pour faire un jeu">
+</head>
+<body>
+<header>
+  <img src="img/header_hitbox.png" alt="">
+  <nav id="socials">
+    <a target="_blank" href="https://discord.gg/ptmJWfd"><img src="img/discord.png" alt="Discord"></a>
+    <a target="_blank" href="https://www.twitch.tv/hitboxmakers"><img src="img/twitch.png" alt="Twitch"></a>
+    <a target="_blank" href="https://www.youtube.com/channel/UCt32L1Ao_KXadv-tu3z-IDQ"><img src="img/yt.svg" alt="Youtube"></a>
+    <a target="_blank" href="https://twitter.com/hitboxmakers"><img src="img/twitter.svg" alt="Twitter"></a>
+    <a target="_blank" href="https://hitboxmakers.itch.io/"><img src="img/itchio.svg" alt="Itch.io"></a>
+  </nav>
+</header>
+
+<div class="content">
+  <div class="helloasso-div">
+    <a href="https://www.helloasso.com/associations/palo-altours/evenements/hitbox-makers-2021" target="_blank">
+      <button class="helloasso-btn">INSCRIPTION</button>
+    </a>
+  </div>
+  <div class="section">
+    <div>
+      <h2>Qui sommes nous ?</h2>
+      <p>Hitbox Makers est une action de <a
+            href="https://paloaltours.org"
+            target="_blank">Palo Altours</a> qui organise des événements autour du développement de jeux vidéo et de jeux de plateau, toujours dans une ambiance festive !
+      </p>
+    </div>
+    <div>
+      <img src="img/palo.png" alt="Logo Palo Altours" height="200px">
+    </div>
+  </div>
+  <div class="section">
+    <div><img src="img/animationHitBox.gif" alt="Logo Jam"></div>
+    <div>
+      <h2>Notre prochaine Game Jam !</h2>
+      <p>Du <strong>Vendredi 9 Avril</strong> à partir de 19h au
+        <strong>Dimanche 11 à 19h</strong> ! Retrouvez-nous sur notre Discord pour un week-end plein de bonne humeur !
+      </p>
+      <p>Que vous soyez débutant ou expérimenté, seul ou en équipe, développeur, musicien, graphiste, auteur, ou simple joueur venez participer !</p>
+      <p>Un atelier d'initiation sera organisé le Samedi Matin !</p>
+    </div>
+  </div>
+  <div class="helloasso-div">
+    <a href="https://www.helloasso.com/associations/palo-altours/evenements/hitbox-makers-2021" target="_blank">
+      <button class="helloasso-btn">INSCRIPTION</button>
+    </a>
+  </div>
+  <div class="section">
+    <div>
+      <h2>Des créations ?</h2>
+      <p>
+        De nombreux jeux ont été réalisé, et vous pouvez en retrouver une partie sur notre <a target="_blank"
+            href="https://hitboxmakers.itch.io/">Itch.io</a> !
+      </p>
+    </div>
+    <div>
+      <img src="img/screen_itchio.PNG" alt="Logo Palo Altours" height="200px">
+    </div>
+  </div>
+  <div class="section">
+    <div>
+      <img src="img/jamdelaloose.png" alt="Logo Jam de la loose">
+    </div>
+    <div>
+      <h2>Les Jams de la Loose</h2>
+      <p>
+        Le dernier dimanche du mois nous organisons aussi les Jam de la Loose ! <br>
+        Le défi ? 1h pour réaliser un jeu ! <br>
+        Retrouvez les infos sur le site des <a href="https://jamdelaloose.fr" target="_blank">Jams de la Loose</a>
+      </p>
+    </div>
+  </div>
+</div>
+<footer>
+  <br> Fuelled with love by <a href="https://paloaltours.org" target="_blank">Palo Altours</a>.
+</footer>
+</body>
+</html>
